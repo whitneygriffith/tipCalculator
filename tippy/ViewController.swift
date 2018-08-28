6d8f45f3f59bcb514c92ee7f66d336600ca26746
Whitney Griffith <whitney.griffith@bison.howard.edu>
2018-08-28 14:18:38 -0400

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Whitney Griffith on 8/28/18.
+//  Copyright © 2018 Whitney Griffith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
